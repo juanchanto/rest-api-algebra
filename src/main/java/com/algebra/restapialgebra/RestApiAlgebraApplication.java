5fc5aa2137b55628129083a86138ab4f3928bb44
juanchanto <juanchanto96@gmail.com>
2023-04-02 18:25:10 -0600

    Initial commit - Backend

---
+++ src/main/java/com/algebra/restapialgebra/RestApiAlgebraApplication.java
@@ -0,0 +1,13 @@
+package com.algebra.restapialgebra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiAlgebraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiAlgebraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algebra/restapialgebra/RestApiAlgebraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algebra.restapialgebra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiAlgebraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
